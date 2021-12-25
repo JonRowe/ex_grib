09a5716f62bd61903702de64c818490014eeb6dc
Jon Rowe <hello@jonrowe.co.uk>
2021-12-25 16:57:58 +0000

    Add identification parsing

--- lib/ex_grib/grib2.ex
+++ lib/ex_grib/grib2.ex
@@ -5,7 +5,11 @@ defmodule ExGrib.Grib2 do
   See: https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/
   """
 
+  alias ExGrib.Grib2.Centre
   alias ExGrib.Grib2.Discipline
+  alias ExGrib.Grib2.GribType
+  alias ExGrib.Grib2.ProductionStatus
+  alias ExGrib.Grib2.SubCentre
 
   @spec header(binary()) :: {:ok, atom(), integer(), binary()} | :error
   def header(
@@ -16,4 +20,42 @@ defmodule ExGrib.Grib2 do
   end
 
   def header(_), do: :error
+
+  @spec identification(binary()) ::
+          {:ok, integer(), integer(), atom(), atom(), integer(), atom(), integer(), integer(),
+           integer(), integer(), integer(), integer(), atom(), atom(), integer()}
+          | :error
+  def identification(<<
+        size::integer-size(32),
+        number::integer,
+        centre_id::integer-size(16),
+        sub_centre_id::integer-size(16),
+        2,
+        local_version::integer(),
+        reference_time_significance::integer(),
+        year::integer-size(16),
+        month::integer(),
+        day::integer(),
+        hour::integer(),
+        minute::integer(),
+        second::integer(),
+        production_status::integer(),
+        type::integer(),
+        possible_reserved::binary()
+      >>) do
+    rest =
+      if size > 21 do
+        reserved_size = size - 21
+        <<_reserved::binary-size(reserved_size), remaining::binary>> = possible_reserved
+        remaining
+      else
+        possible_reserved
+      end
+
+    {:ok, size, number, Centre.get(centre_id), SubCentre.get(centre_id, sub_centre_id),
+     local_version, reference_time_significance, year, month, day, hour, minute, second,
+     ProductionStatus.get(production_status), GribType.get(type), rest}
+  end
+
+  def identification(_), do: :error
 end
--- test/ex_grib/grib2_test.exs
+++ test/ex_grib/grib2_test.exs
@@ -16,4 +16,17 @@ defmodule ExGrib.Grib2Test do
       assert :error = Grib2.header(<<"NOTAGRIB">>)
     end
   end
+
+  describe "identification/1" do
+    test "it returns the identification details of the grib" do
+      {:ok, _, _, rest} = Grib2.header(file_contents("gfs_25km.grb2"))
+
+      {:ok, 21, 1, :us_national_weather_service_ncep_wmc, :unknown, 1, 1, 2021, 12, 12, 12, 0, 0,
+       :operational_products, :forecast_products, _} = Grib2.identification(rest)
+    end
+
+    test "it errors on an unrecognised section" do
+      assert :error = Grib2.identification(<<"NOTAGRIB">>)
+    end
+  end
 end
