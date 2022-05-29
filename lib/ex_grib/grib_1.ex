bb3f877e1d0337c4af2dab37cf18af6c69401722
Jon Rowe <hello@jonrowe.co.uk>
2022-05-29 16:31:47 +0100

    Add grib1 detection using headers

--- lib/ex_grib/grib_1.ex
+++ lib/ex_grib/grib_1.ex
@@ -5,6 +5,14 @@ defmodule ExGrib.Grib1 do
 
   alias ExGrib.Grib1.Section0
 
+  @spec detect(binary()) :: boolean()
+  def detect(binary) do
+    case Section0.parse(binary) do
+      {:ok, _, _} -> true
+      :error -> false
+    end
+  end
+
   @spec header(Section0.input()) :: Section0.t()
   def header(input), do: Section0.parse(input)
 end
--- test/ex_grib/grib_1_test.exs
+++ test/ex_grib/grib_1_test.exs
@@ -8,6 +8,16 @@ defmodule ExGrib.Grib1Test do
 
   doctest ExGrib.Grib1
 
+  describe "detect/1" do
+    test "it returns true when binary contains a grib1 header" do
+      assert true == Grib1.detect(file_contents("forecast.grb"))
+    end
+
+    test "it returns false otherwise" do
+      assert false == Grib1.detect(file_contents("gfs_25km.grb2"))
+    end
+  end
+
   describe "header/1" do
     test "it returns the header of the grib" do
       assert {:ok, %Section0{file_size: 7526}, _} = Grib1.header(file_contents("forecast.grb"))
