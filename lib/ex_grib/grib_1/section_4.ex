bcc7c725e0a21165b860d0259705f47a382c1775
Jon Rowe <hello@jonrowe.co.uk>
2022-06-01 09:30:31 +0100

    Add data chunking to section 4

--- lib/ex_grib/grib_1/section_4.ex
+++ lib/ex_grib/grib_1/section_4.ex
@@ -19,6 +19,7 @@ defmodule ExGrib.Grib1.Section4 do
 
   defstruct binary_scale_factor: :not_parsed,
             bits_per_value: :not_parsed,
+            data: :not_parsed,
             data_flag: :not_parsed,
             reference_value: :not_parsed,
             section_length: :not_parsed
@@ -39,12 +40,15 @@ defmodule ExGrib.Grib1.Section4 do
       >>) do
     remaining_octets = section_length - 11
 
-    <<_variable::binary-size(remaining_octets), rest::binary()>> = more
+    <<data::binary-size(remaining_octets), rest::binary()>> = more
+
+    table_11 = Table11.get(data_flag)
 
     section = %__MODULE__{
       binary_scale_factor: binary_scale_factor,
       bits_per_value: bits_per_value,
-      data_flag: Table11.get(data_flag),
+      data: parse_data(data, bits_per_value, table_11),
+      data_flag: table_11,
       reference_value: reference_value,
       section_length: section_length
     }
@@ -53,4 +57,29 @@ defmodule ExGrib.Grib1.Section4 do
   end
 
   def parse(_), do: :error
+
+  defp parse_chunk(data, n) when byte_size(data) * 8 >= n do
+    <<chunk::binary-size(n)-unit(1), rest::binary()-unit(1)>> = data
+    [chunk | parse_chunk(rest, n)]
+  end
+
+  defp parse_chunk(_, _), do: []
+
+  defp parse_data(data, n, %Table11{additional_flags_at_section_4_octect_14: false} = flags) do
+    case flags do
+      %Table11{grid_or_sphere: :grid, simple_or_complex: :simple} ->
+        # From octet 12 onwards, e.g. straight up data.
+        parse_chunk(data, n)
+    end
+  end
+
+  # probably not 0 0 but will serve as a point to start when octect 14 does exist
+  defp parse_data(
+         <<0, 0, more_flags::binary-size(1), _data::binary()>>,
+         _n,
+         %Table11{additional_flags_at_section_4_octect_14: true} = flags
+       ) do
+    _table_11 = Table11.get_additional(flags, more_flags)
+    []
+  end
 end
