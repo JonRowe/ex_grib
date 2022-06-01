60e34311eab12225bea9683534b8dec4328bddf9
Jon Rowe <hello@jonrowe.co.uk>
2022-06-01 09:29:54 +0100

    Decode latitude and longitude in grid

--- lib/ex_grib/grib_1/grids/latitude_longitude_grid.ex
+++ lib/ex_grib/grib_1/grids/latitude_longitude_grid.ex
@@ -19,15 +19,15 @@ defmodule ExGrib.Grib1.Grids.LatitudeLongitudeGrid do
         # Nj number of points along a meridian
         nj::unsigned-size(16),
         # La1 latitude of first grid point
-        latitude_of_first_grid_point::signed-size(24),
+        latitude_of_first_grid_point::binary-size(3),
         # Lo1 longitude of first grid point
-        longitude_of_first_grid_point::signed-size(24),
+        longitude_of_first_grid_point::binary-size(3),
         # Resolution and component flags (see Code table 7)
         resolution_and_component_flags::integer(),
         # La2 latitude of last grid point
-        latitude_of_last_grid_point::signed-size(24),
+        latitude_of_last_grid_point::binary-size(3),
         # Lo2 longitude of last grid point
-        longitude_of_last_grid_point::signed-size(24),
+        longitude_of_last_grid_point::binary-size(3),
         # Di i direction increment
         i_direction_increment::unsigned-size(16),
         # Dj j direction increment
@@ -42,14 +42,22 @@ defmodule ExGrib.Grib1.Grids.LatitudeLongitudeGrid do
     %__MODULE__{
       i_direction_increment: i_direction_increment,
       j_direction_increment: j_direction_increment,
-      latitude_of_first_grid_point: latitude_of_first_grid_point,
-      latitude_of_last_grid_point: latitude_of_last_grid_point,
-      longitude_of_first_grid_point: longitude_of_first_grid_point,
-      longitude_of_last_grid_point: longitude_of_last_grid_point,
+      latitude_of_first_grid_point: coord(latitude_of_first_grid_point),
+      latitude_of_last_grid_point: coord(latitude_of_last_grid_point),
+      longitude_of_first_grid_point: coord(longitude_of_first_grid_point),
+      longitude_of_last_grid_point: coord(longitude_of_last_grid_point),
       ni: ni,
       nj: nj,
       resolution_and_component_flags: resolution_and_component_flags,
       scanning_mode: scanning_mode
     }
+  end
+
+  defp coord(<<0::size(1)-unit(1), value_in_milli_degrees::integer-size(23)-unit(1)>>) do
+    value_in_milli_degrees / 1000.0
+  end
+
+  defp coord(<<1::size(1)-unit(1), value_in_milli_degrees::integer-size(23)-unit(1)>>) do
+    -1 * (value_in_milli_degrees / 1000.0)
   end
 end
