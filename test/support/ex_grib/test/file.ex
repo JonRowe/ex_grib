828ffe4e879650b47b25c64bcadea9b787364785
Jon Rowe <hello@jonrowe.co.uk>
2022-11-16 12:42:47 +0000

    Add primative csv parsing to test support

--- test/support/ex_grib/test/file.ex
+++ test/support/ex_grib/test/file.ex
@@ -17,11 +17,38 @@ defmodule ExGrib.Test.File do
     |> process(opts)
   end
 
+  defp process(data, [{:parse, :csv}]) do
+    data
+    |> String.split("\n", trim: true)
+    |> Enum.map(&read_csv_line/1)
+  end
+
+  defp process(_, [{:parse, :csv} | _]),
+    do: raise(ArgumentError, "Combining csv parsing with anything else is not supported")
+
   defp process(data, [{:skip, [{:octets, n}]} | rest]), do: process(skip_bytes(n, data), rest)
   defp process(data, []), do: data
 
   defp skip_bytes(n, input) do
     <<_skip::binary-size(n), rest::binary()>> = input
     rest
+  end
+
+  defp read_csv_line(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(fn element ->
+      cond do
+        element =~ ~r/".*"/ ->
+          String.trim(element, "\"")
+
+        element =~ ~r/-?\d+\.\d+/ ->
+          {value, ""} = Float.parse(element)
+          value
+
+        true ->
+          raise ArgumentError, "Unexpected value in parsing csv #{inspect(element)}"
+      end
+    end)
   end
 end
